91fb63d23c1bd36d4142ae053c43af14218cafe8
LIN XIANYING <soullxy@qq.com>
2017-12-05 07:16:49 +0800

    Update mipslabmain.c

--- time4io/mipslabmain.c
+++ time4io/mipslabmain.c
@@ -55,19 +55,9 @@ int main(void) {
 	SPI2CONSET = 0x8000;
 	
 	display_init();
-	display_string(0, "KTH/ICT lab");
-	display_string(1, "in Computer");
-	display_string(2, "Engineering");
-	display_string(3, "Welcome!");
 	display_update();
+	labinit(); 
+	guessgod(); 
 	
-	display_image(96, icon);
-	
-	labinit(); /* Do any lab-specific initialization */
-
-	while( 1 )
-	{
-	  labwork(); /* Do lab-specific things again and again */
-	}
 	return 0;
 }
