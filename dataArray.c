bfd1fb3191858eff3319fdd07419896fb8263e2d
lingqi1219 <ziyxia1219@gmail.com>
2017-12-13 16:12:19 +0100

    Update dataArray.c

--- dataArray.c
+++ dataArray.c
@@ -1,13 +1,10 @@
 
-#define Screen 3
+#define Screen 4
 #define State 9
 #define NUMBER_OF_STRINGS = 20 
  
 signed int current; 
 
- 
-int main ()
-{
     int gameData[Screen][State] = {{1, 1, 1, 0,0,0,0, 0,0},//command,dir1,dir2,btn1234,swt12, 
                               {1, 2, 2, 0,0,0,0, 0,0},
                               {1, 3, 3, 0,0,0,0, 0,0},
@@ -22,23 +19,54 @@ int main ()
     strcpy(strs[1], "display1");
     strcpy(strs[2], "display2");
     strcpy(strs[3], "display3"); 
-                             
-}
  
-void resetData(int (*arr)[State])
+void resetData(int (*gameData[State])
 {
     int i, j;
-    for (i = 0; i < ROWS; i++)
+    for (i = 0; i < Screen; i++)
     {
-        for (j = 4; j < COLS; j++)
+        for (j = 4; j < State; j++)
         {
-            (*arr)[j] = 0;
+            (*gameData)[j] = 0;
         }
-        arr++;
+        gameData++;
     }
 }
+
+void useButtons(int * pastButton) {
+    int btns = getbtns2();
+        if (btns==1){
+            gameData[current][4] = 1;
+        }else 
+        if(btn==2){
+            gameData[current][5] = 1;
+        }else 
+        if(btn==3){
+            gameData[current][6] = 1;
+        }else
+        if(btn==3){
+            gameData[current][7] = 1;} 
+ 
+ * pastButton = btns;
+}
+
+void changeScreen() { 
+		      for (i = 0; i < 3; i++){
+        display_string(i,displayList[4*current][i]);
+        }
+    } 
  
 void game(void) {
     current = 0;
-    while (1) {}
+    int pastButton = 0;
+    while (1) {
+        
+        if (dataArray[current][1] != 0) {
+           current = ;
+        }
+    
+        changeScreen(current);
+        resetData(current);
+        useButtons(& pastButton);
+    }
 }
